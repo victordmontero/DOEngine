e3da6c3a1b5c2c03f678613bcd0e34ca6c834850
aneury1 <aneury1@gmail.com>
2020-10-29 18:58:52 -0700

    remove all stuff that does not fit

--- src/GameState/tetrisState1.cpp
+++
@@ -1,6 +0,0 @@
-#include "OneLoneCodeTetrisState.h"
-
-void OneLoneCodeTetrisState::OnEnter(){}
-void OneLoneCodeTetrisState::OnExit(){}
-void OneLoneCodeTetrisState::Update(float elapsed){}
-void OneLoneCodeTetrisState::Render(){}
