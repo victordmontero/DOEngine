8b02c5924a88599bb0096d5bbe1977aff7282559
aneury <baneury@bairesdev.com>
2023-07-26 08:06:52 -0400

    Random Int

--- src/includes/StandardFuncs.h
+++ src/includes/StandardFuncs.h
@@ -1,11 +1,19 @@
 #pragma once 
-
+#include <random>
 
 template<typename V1, typename V2>
 struct Optional{
    V1 v1;
    V2 v2;
 };
+
+inline int getRandomInt(int startRange, int endRange){
+  std::random_device rd;
+  std::uniform_int_distribution<int>dist(startRange, endRange);
+  auto ret = dist(rd);
+  return ret;
+}
+
 
 
 
