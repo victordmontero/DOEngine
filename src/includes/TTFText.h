34d04ecf236e7013cc73d5f4ab8e4f13eeb725b1
victordmontero <victordanielmontero@gmail.com>
2023-11-25 21:44:56 +0100

    Modified TTFont class to use doengine::Color instead of SDL's

--- src/includes/TTFText.h
+++ src/includes/TTFText.h
@@ -1,32 +1,39 @@
 #pragma once
 
 #include "DOEngine.h"
+#include "Geometric.h"
+#include "abstract/Font.h"
 
-class TTFText
+using doengine::Font;
+
+class TTFText : public Font
 {
 
-    TTFText():font(NULL){
+    TTFText() : font(NULL)
+    {
         color.r = 0xAA;
         color.g = 0xFF;
         color.b = 0xBB;
         color.a = 255;
     }
-    static TTFText *singleton;
-    
-    TTF_Font *font;
+    static TTFText* singleton;
+
+    TTF_Font* font;
     SDL_Color color;
 
-    public:
-
-    static TTFText* get(){
-        if(singleton==nullptr)
-           singleton = new TTFText();
+  public:
+    static TTFText* get()
+    {
+        if (singleton == nullptr)
+            singleton = new TTFText();
         return singleton;
     }
 
-    inline void setColor(SDL_Color color){this->color = color;}
-    void setFont(const std::string& path, int fntsize= 23);
-    SDL_Texture *createText(const std::string& text, SDL_Renderer *render);
-    void DrawText(const char *text, int x, int y, SDL_Renderer *render);
-
+    void setColor(doengine::Color color) override
+    {
+        this->color = static_cast<SDL_Color>(color.rgba);
+    }
+    void setFont(const std::string& path, int fntsize = 23) override;
+    SDL_Texture* createText(const std::string& text, SDL_Renderer* render);
+    void DrawText(const char* text, int x, int y, SDL_Renderer* render);
 };
