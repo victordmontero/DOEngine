81f7dff605333df310841caa9e1a16633b7b7c5b
victordmontero <victordanielmontero@gmail.com>
2026-07-16 22:37:52 +0200

    Fixing SDL includes

--- includes/DOEngine_SDL_includes.h
+++ includes/DOEngine_SDL_includes.h
@@ -33,18 +33,16 @@
 #ifndef DOENGINE_SDL_INCLUDES_HPP_DEFINED
 #define DOENGINE_SDL_INCLUDES_HPP_DEFINED
 
-
+#include <GL/gl.h>
+#include <GL/glext.h>
 #include <SDL2/SDL.h>
 #include <SDL2/SDL_error.h>
 #include <SDL2/SDL_events.h>
 #include <SDL2/SDL_joystick.h>
 #include <SDL2/SDL_log.h>
-#include <SDL2/SDL_image.h>
-#include <SDL2/SDL_ttf.h>
-#include <SDL2/SDL_mixer.h>
 #include <SDL2/SDL_opengl.h>
-#include <GL/gl.h>
-#include <GL/glext.h>
-
+#include <SDL_image.h>
+#include <SDL_mixer.h>
+#include <SDL_ttf.h>
 
 #endif
